cd5d57328b8875b0852ea49b3e5f202c6ef18e11
Justin Hiester <jhiester@gmail.com>
2019-03-21 12:12:58 -0700

    motor demonstration works

--- motor_switch_test/motor_switch_test.ino
+++ motor_switch_test/motor_switch_test.ino
@@ -1,7 +1,8 @@
 #define MOTOR_CW_PIN 14
 #define MOTOR_CCW_PIN 12
 #define SWITCH_ONE_PIN 13
-#define SWITCH_TWO_PIN 15
+#define SWITCH_TWO_PIN 5
+
 
 void setup() {
   pinMode(SWITCH_ONE_PIN, INPUT);
@@ -12,28 +13,44 @@ void setup() {
   digitalWrite(MOTOR_CW_PIN, LOW);
   digitalWrite(MOTOR_CCW_PIN, LOW);
 }
-     
-void loop() {
-  Serial.println("Delay...");
-  delay(3000);
-  if ((digitalRead(SWITCH_ONE_PIN) == HIGH) && (digitalRead(SWITCH_TWO_PIN) == LOW)) {    
+
+void open() {
+  while (digitalRead(SWITCH_TWO_PIN) == HIGH) {
     digitalWrite(MOTOR_CW_PIN, HIGH);
     digitalWrite(MOTOR_CCW_PIN, LOW);
-    Serial.println("CW");
-  } else if ((digitalRead(SWITCH_ONE_PIN) == LOW) && (digitalRead(SWITCH_TWO_PIN) == HIGH)) {    
-    digitalWrite(MOTOR_CW_PIN, LOW);    
-    digitalWrite(MOTOR_CCW_PIN, HIGH);
-    Serial.println("CCW");
-  } else if ((digitalRead(SWITCH_ONE_PIN) == LOW) && (digitalRead(SWITCH_TWO_PIN) == LOW)) {
-    digitalWrite(MOTOR_CW_PIN, LOW);    
-    digitalWrite(MOTOR_CCW_PIN, LOW);
-    Serial.println("NEITHER");
+    yield();
   }
+
+  stop();
+  delay(2000);
 }
 
-/*
-void setup() {
+
+void close() {
+  while (digitalRead(SWITCH_ONE_PIN) == HIGH) {
+    digitalWrite(MOTOR_CW_PIN, LOW);
+    digitalWrite(MOTOR_CCW_PIN, HIGH);
+    yield();
+  }
+
+  stop();
+  delay(2000);
 }
-     
+
+
+void stop() {
+  digitalWrite(MOTOR_CW_PIN, LOW);
+  digitalWrite(MOTOR_CCW_PIN, LOW);
+}
+
+
 void loop() {
-}*/
+  delay(3500);
+  
+  if ((digitalRead(SWITCH_ONE_PIN) == LOW) && (digitalRead(SWITCH_TWO_PIN) == HIGH))
+    open();
+  else if ((digitalRead(SWITCH_ONE_PIN) == HIGH) && (digitalRead(SWITCH_TWO_PIN) == LOW))
+    close();
+  else if ((digitalRead(SWITCH_ONE_PIN) == HIGH) && (digitalRead(SWITCH_TWO_PIN) == HIGH) || (digitalRead(SWITCH_ONE_PIN) == LOW) && (digitalRead(SWITCH_TWO_PIN) == LOW))
+    stop();
+}
